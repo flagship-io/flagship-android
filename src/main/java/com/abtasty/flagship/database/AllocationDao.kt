421e056a12c8c30ae308f2bab330695cf2878a68
raf-abtasty <raphael@abtasty.com>
2019-10-24 17:03:38 +0200

    add allocDao

---
+++ src/main/java/com/abtasty/flagship/database/AllocationDao.kt
@@ -0,0 +1,18 @@
+package com.abtasty.flagship.database
+
+import android.renderscript.Allocation
+import androidx.room.*
+
+@Dao
+interface AllocationDao {
+
+//    @Insert(onConflict = OnConflictStrategy.REPLACE)
+//    fun insertAllocation(allocationData: AllocationData) : Long
+//
+//    @Query("Delete From allocations WHERE visitorId = :visitorId AND customVisitorId = :customVisitorId")
+//    fun deleteAllAllocations(visitorId : String, customVisitorId : String)
+//
+//    @Query("Select * FROM allocations WHERE visitorId = :visitorId AND customVisitorId = :customVisitorId AND variationId = :variationId")
+//    fun getAllocation(visitorId : String, customVisitorId : String, variation : String) : AllocationData
+
+}
