74678d3834f46e33bf4be6c7cffd2a86c6a38132
raf-abtasty <raphael@abtasty.com>
2019-10-24 16:39:53 +0200

    fix day of month in genvisitorId, upgrade 1.0.2

--- src/main/java/com/abtasty/flagship/utils/Utils.kt
+++ src/main/java/com/abtasty/flagship/utils/Utils.kt
@@ -33,14 +33,14 @@ class Utils {
 
             val sharedPref = context.getSharedPreferences("_Flagship", Context.MODE_PRIVATE)
             var visitorId = sharedPref.getString("visitorId", "")
-            if (visitorId.isEmpty()) {
+            if (visitorId == null || visitorId.isEmpty()) {
 
                 val cal = Calendar.getInstance()
                 val min = 10000
                 val max = 99999
                 val random = Random().nextInt(max - min + 1) + min
                 visitorId = "${cal.get(Calendar.YEAR)}" +
-                        "${cal.get(Calendar.MONTH)}" +
+                        "${cal.get(Calendar.MONTH) + 1}" +
                         "${cal.get(Calendar.DAY_OF_MONTH)}" +
                         "${cal.get(Calendar.HOUR_OF_DAY)}" +
                         "${cal.get(Calendar.MINUTE)}" +
