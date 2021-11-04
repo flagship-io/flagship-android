e36e41e01ecc4827d71361f5ea73e3a0f1c6094f
Chadi <chadilaoulaou@gmail.com>
2021-11-04 23:02:06 +0100

    fixing a test

--- app/src/androidTest/java/com/abtasty/flagshipqa/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/abtasty/flagshipqa/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.abtasty.ezqa", appContext.packageName)
+        assertEquals("com.abtasty.flagshipqa", appContext.packageName)
     }
 }
