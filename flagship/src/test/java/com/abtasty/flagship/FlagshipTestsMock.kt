893e785b4f4227210a15fe00d3b7f39319066007
= <raphael@abtasty.com>
2025-02-11 12:28:30 +0100

    httpmanager 1

--- flagship/src/test/java/com/abtasty/flagship/FlagshipTestsMock.kt
+++ flagship/src/test/java/com/abtasty/flagship/FlagshipTestsMock.kt
@@ -1,14 +1,27 @@
 package com.abtasty.flagship
 
+import com.abtasty.flagship.AFlagshipTest.Companion._API_KEY_
+import com.abtasty.flagship.AFlagshipTest.Companion._ENV_ID_
+import com.abtasty.flagship.main.Flagship
+import com.abtasty.flagship.main.FlagshipConfig
 import com.abtasty.flagship.model.CampaignMetadata
 import com.abtasty.flagship.model.Variation
 import com.abtasty.flagship.model.VariationGroupMetadata
 import com.abtasty.flagship.model.VariationMetadata
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.runBlocking
 import org.json.JSONObject
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.RuntimeEnvironment
+import org.robolectric.annotation.Config
+import org.robolectric.annotation.LooperMode
 
-class FlagshipTestsMock : AFlagshipTest() {
+@RunWith(RobolectricTestRunner::class)
+@LooperMode(LooperMode.Mode.INSTRUMENTATION_TEST)
+@Config(sdk = [24])
+class FlagshipTestsMock {
         @Test
         fun variations() {
             val v = Variation.parse(JSONObject("{}"), false, VariationGroupMetadata("", "", CampaignMetadata()))
@@ -29,5 +42,13 @@ class FlagshipTestsMock : AFlagshipTest() {
                 VariationMetadata("", "", false, 100, VariationGroupMetadata("", "", CampaignMetadata()))
             )
             assertTrue(flags == null)
+        }
+
+        @Test
+        fun initHttpClient() {
+            runBlocking {
+                Flagship.start(RuntimeEnvironment.getApplication(), _ENV_ID_, _API_KEY_, FlagshipConfig.DecisionApi()).await()
+            }
+            assert(Flagship.getStatus() == Flagship.FlagshipStatus.INITIALIZED)
         }
 }
